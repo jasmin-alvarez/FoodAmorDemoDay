ad95f20426dc942b67c9aef5bcd7d93755d18f0a
Jasmin Alvarez <70730021+Jasmin-L-Alvarez@users.noreply.github.com>
2021-12-13 23:02:41 -0500

    Update config.php

--- public/assets/js/config.php
+++ public/assets/js/config.php
@@ -2,8 +2,8 @@
 require_once('vendor/autoload.php');
 
 $stripe = [
-  "secret_key"      => "sk_test_51IfdITEOiwnwvfU2NMSbfVF1oEW8Cle8Hcuy6UGtyonCoFgCLnZ8icdGITujL3mzAZ838PuJ6BUCt2nkOlErrhcB0025Qe4UFf",
-  "publishable_key" => "pk_test_51IfdITEOiwnwvfU2IPdzwcXxZpsb83hAGYiR6W4KzMYBgnKScBI2XapUOh6VfMvdAfO93fLmX7I34g4gxFtkAhwh00ojN8QYo2",
+  "secret_key"      => "sk_test_",
+  "publishable_key" => "pk_test_",
 ];
 
 \Stripe\Stripe::setApiKey($stripe['secret_key']);
